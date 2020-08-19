49782f678477fb46e6843d3f66547e53de849c6e
Cook1619 <cook6348@gmail.com>
2020-08-19 15:13:07 -0500

    Setting up simple apollo server graphql scema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,12 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
+
 const app = express();
 const port: number = 9000;
 
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello from express!!!!</h1>");
-});
-
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1)[0]);
-    }
-  }
-
-  return res.send("failed to deleted listing");
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
