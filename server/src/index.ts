a83cba43269911265453c55cd00e887f8e1aa223
Cook1619 <cook6348@gmail.com>
2020-08-18 19:48:13 -0500

    Adding some dummy data and setting up routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listings";
 const app = express();
 const port: number = 9000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello from express!!!!</h1>");
+});
+
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1)[0]);
+    }
+  }
+
+  return res.send("failed to deleted listing");
 });
 
 app.listen(port);
